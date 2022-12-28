51eb9e537879e0b1157b59862f1927dbbf05448a
Yakup Unyilmaz <yakupunyilmaz@gmail.com>
2022-12-28 18:04:11 +0100

    add examples

---
+++ PersonRecord.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace RecordType;
+
+public record PersonRecord(string FullName, DateOnly DateOfBirth)
+{
+    protected virtual bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append($"Name is: {FullName}");
+        builder.Append($"Date Of Birth is: {DateOfBirth}");
+        return true;
+    }
+}
+
+// public record Person
+// {
+//     public string FullName { get; init; } = default!;
+//     public DateOnly DateOfBirth { get; init; } = default!;
+// }
+
+public record struct PersonStruct(string FullName, DateOnly DateOfBirth);
+
+public class PersonClass
+{
+    public string FullName { get; init; } = default!;
+    public DateOnly DateOfBirth { get; init; } = default!;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RecordType;
+
+var yakup = new PersonRecord("Yakup", new DateOnly(2000, 1, 1));
+var yakup2 = new PersonRecord("Yakup", new DateOnly(2000, 1, 1));
+
+var yakupOlder = yakup with { DateOfBirth = new DateOnly(1999, 1, 1) };
+
+var (name, dateOfBirth) = yakup;
+Console.WriteLine($"{name} {dateOfBirth}");
+
+Console.WriteLine(yakup);
+Console.WriteLine(yakupOlder);
+
+Console.WriteLine(yakup == yakup2);
+
+var yakupClass = new PersonClass()
+{
+    FullName = "Yakup",
+    DateOfBirth = new DateOnly(2000, 1, 1)
+};
+
+var yakupClass2 = new PersonClass()
+{
+    FullName = "Yakup",
+    DateOfBirth = new DateOnly(2000, 1, 1)
+};
+
+Console.WriteLine(yakupClass == yakupClass2);
